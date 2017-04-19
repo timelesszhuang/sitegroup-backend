f6de377bc8d7c6c51ff67843378efe193599974b
2301887641 <sgz19861126>
2017-04-19 11:34:52 +0800

    用户验证

--- application/common/controller/Common.php
+++ application/common/controller/Common.php
@@ -21,7 +21,7 @@ class Common extends Controller
     function __construct()
     {
         parent::__construct();
-        header("Access-Control-Allow-Origin:" . $_SERVER['HTTP_ORIGIN']);
+//        header("Access-Control-Allow-Origin:" . $_SERVER['HTTP_ORIGIN']);
         header("Access-Control-Allow-Credentials: true ");
     }
 
@@ -61,12 +61,13 @@ class Common extends Controller
      */
     function resultArray($msg=0,$stat='',$data=0)
     {
+        header("Content-type: text/html; charset=utf-8");
         if(empty($stat)){
             $status="success";
         }else{
             $status="failed";
         }
-        exit(josn_encode([
+        exit(json_encode([
             'status' => $status,
             'data' => $data,
             'msg' => $msg
