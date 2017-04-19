dff8f5cf5ebead3904469fddc729078e223ff3bd
timelessz <834916321@qq.com>
2017-04-19 23:16:19 +0800

    测试跨域操作

--- application/common/controller/Common.php
+++ application/common/controller/Common.php
@@ -22,8 +22,7 @@ class Common extends Controller
     function __construct()
     {
         parent::__construct();
-//        header("Access-Control-Allow-Origin:" . $_SERVER['HTTP_ORIGIN']);
-        header("Access-Control-Allow-Origin:*");
+        header("Access-Control-Allow-Origin:" . $_SERVER['HTTP_ORIGIN']);
         header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
         header("Access-Control-Allow-Credentials: true ");
 /*        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
