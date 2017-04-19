912c8fff405cea61f0a87dfb415ae91d0bf0f9c1
timelessz <834916321@qq.com>
2017-04-19 23:30:13 +0800

    CORS 跨域操作

--- application/common/controller/Common.php
+++ application/common/controller/Common.php
@@ -34,7 +34,7 @@ class Common extends Controller
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
                 // may also be using PUT, PATCH, HEAD etc
-                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+                header("Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS");
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
                 header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
             exit(0);
