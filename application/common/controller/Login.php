c7840c8bbecf37baddc79cc6fede326e9c6e73aa
2301887641 <sgz19861126>
2017-04-20 11:49:15 +0800

    跨域

--- application/common/controller/Login.php
+++ application/common/controller/Login.php
@@ -15,6 +15,30 @@ use think\Validate;
 class Login extends Controller
 {
     /**
+     * 本地测试开启下 允许跨域ajax 获取数据
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                // may also be using PUT, PATCH, HEAD etc
+                header("Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS");
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            exit(0);
+        }
+    }
+    /**
      * 执行第一次的登陆操作
      * @access public
      * @auther guozhen
