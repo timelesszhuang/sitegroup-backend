a02f947ca6239d34dd231845776fb30222b3344c
2301887641 <sgz19861126>
2017-04-21 15:30:56 +0800

    save

--- application/common/controller/Company.php
+++ application/common/controller/Company.php
@@ -9,13 +9,32 @@
 namespace app\common\controller;
 class Company extends Common
 {
+    /**
+     * 获取公司信息
+     * @return array
+     * @auther guozhen
+     */
     public function index()
     {
         if($this->request->isGet()){
+            $request=$this->getLimit();
+            return (new \app\common\model\Company)->getCompany($request["limit"],$request["rows"]);
+        }
+    }
 
+    public function add()
+    {
+        if($this->request->isPut()){
+            $rule=[
+                ["name","require","请输入公司名称"],
+                ["artificialperson","require","请输入法人"],
+
+            ];
 
 
         }
+
+
     }
 
 }
