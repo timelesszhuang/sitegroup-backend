1eb529bee1a5044e6a7b60f004f5b0f3d7a0e7ca
2301887641 <sgz19861126>
2017-04-21 09:37:04 +0800

    修复bug

--- application/common/controller/User.php
+++ application/common/controller/User.php
@@ -4,7 +4,7 @@ namespace app\common\controller;
 
 use think\Controller;
 use think\Request;
-class User extends Controller
+class User extends Common
 {
     /**
      * 显示资源列表
--- application/common/model/User.php
+++ application/common/model/User.php
@@ -41,37 +41,36 @@ class User extends Model
     }
 
 
-
     /**
      * 修改密码
      * @param $oldPwd
      * @param $newPwd
      * @return array
      */
-    public function changePwd($oldPwd,$newPwd)
+    public function changePwd($oldPwd, $newPwd)
     {
-        $common=new Common();
-        $userSession=$common->getSessionUser();
-        $user_id=$userSession["user_id"];
-        $user_info=$this::get($user_id);
-        if(empty($user_info)){
-            return ["登录超时,请重新登录",'failed',''];
+        $common = new Common();
+        $userSession = $common->getSessionUser();
+        $user_id = $userSession["user_id"];
+        $user_info = $this::get($user_id);
+        if (empty($user_info)) {
+            return ["登录超时,请重新登录", 'failed', ''];
         }
-        if($user_info->pwd!=md5($oldPwd.$user_info->user_name)){
-            return ["原密码错误",'failed',''];
+        if ($user_info->pwd != md5($oldPwd . $user_info->user_name)) {
+            return ["原密码错误", 'failed', ''];
         }
         //原密码和新密码相同
-        if($oldPwd == $newPwd){
-            return ["原密码和新密码不能相同",'failed',''];
+        if ($oldPwd == $newPwd) {
+            return ["原密码和新密码不能相同", 'failed', ''];
         }
         //新密码长度
-        if(strlen($newPwd)<6){
-            return ["新密码不能小于6位",'failed',''];
+        if (strlen($newPwd) < 6) {
+            return ["新密码不能小于6位", 'failed', ''];
         }
-        if(!$this->where(["id"=>$user_id])->update(["pwd"=>md5($newPwd.$user_info->user_name)])){
-            return ["密码修改失败",'failed',''];
+        if (!$this->where(["id" => $user_id])->update(["pwd" => md5($newPwd . $user_info->user_name)])) {
+            return ["密码修改失败", 'failed', ''];
         }
-        return ["密码修改成功",'',''];
+        return ["密码修改成功", '', ''];
     }
 
     /**
@@ -79,20 +78,20 @@ class User extends Model
      * @param $user_info_arr
      * @auther jingzheng
      */
-    public function  setSession($user_info_arr){
-       if($user_info_arr['type']==1){
-            $user_name = "sys_user_name";
-            $id = "sys_id";
-            $name = "sys_name";
-       }else if($user_info_arr['type']==2){
-           $user_name="admin_user_name";
-           $id = "admin_id";
-           $name = "admin_name";
-       }
+    public function setSession($user_info_arr)
+    {
+        $user_name = "sys_user_name";
+        $id = "sys_id";
+        $name = "sys_name";
+        if ($user_info_arr['type'] == 2) {
+            $user_name = "admin_user_name";
+            $id = "admin_id";
+            $name = "admin_name";
+        }
         Session::set($user_name, $user_info_arr["user_name"]);
-        Session::set($id,$user_info_arr["id"]);
-        Session::set($name,$user_info_arr["name"]);
-        Session::set("type",$user_info_arr["type"]);
+        Session::set($id, $user_info_arr["id"]);
+        Session::set($name, $user_info_arr["name"]);
+        Session::set("type", $user_info_arr["type"]);
     }
 
     /**
@@ -102,13 +101,29 @@ class User extends Model
      * @return array
      */
 
-    public function getUser($limit,$rows)
+    public function getUser($limit, $rows)
     {
-        $count=$this->count();
-        $data=$this->limit($limit,$rows)->order("id","desc")->select();
+        $count = $this->count();
+        $data = $this->limit($limit, $rows)->order("id", "desc")->field("id,user_name,type,name,tel,mobile,qq,wechat,email,create_time")->select();
         return [
-            "total"=>$count,
-            "rows"=>$data
+            "total" => $count,
+            "rows" => $data
         ];
+    }
+
+    /**
+     * 获取字段type
+     * @param $value
+     * @return string
+     */
+    public function getTypeAttr($value)
+    {
+        $type = '';
+        if ($value == 1) {
+            $type = "大后台";
+        } else if ($value == 2) {
+            $type = "小后台";
+        }
+        return $type;
     }
 }
