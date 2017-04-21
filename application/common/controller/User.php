01a560b7748a2a4b72d2d594dd8e7f3fab8eacbd
2301887641 <sgz19861126>
2017-04-21 14:14:35 +0800

    local

--- application/common/controller/User.php
+++ application/common/controller/User.php
@@ -4,6 +4,7 @@ namespace app\common\controller;
 
 use think\Controller;
 use think\Request;
+use think\Validate;
 
 class User extends Common
 {
@@ -29,11 +30,38 @@ class User extends Common
      */
     public function read($id)
     {
-        if($this->request->isGet()){
+        if ($this->request->isGet()) {
             $user = new \app\common\model\User;
-            return $user->field("id,user_name,type,name,tel,mobile,qq,wechat,email,create_time")->where(["id"=>$id])->find();
+            return $user->field("id,user_name,type,name,tel,mobile,qq,wechat,email,create_time")->where(["id" => $id])->find();
         }
+    }
 
+    /**
+     * 添加用户
+     * @return array
+     * @auther guozhen
+     */
+    public function add()
+    {
+        if ($this->request->isPost()) {
+            $rule = [
+                ["user_name", "require", "请输入用户名"],
+                ["pwd", "require", "请输入密码"],
+                ["contacks", "require", "请输入联系人"],
+                ["tel", "require", "请输入电话"],
+                ["type_name","require","请选择类型"],
+                ["type","require","请选择类型"]
+            ];
+            $data = $this->request->post();
+            $validate = new Validate($rule);
+            if (!$validate->check($data)) {
+                return $this->resultArray($validate->getError(), 'failed');
+            }
+            if (!\app\common\model\User::create($data)) {
+                return $this->resultArray('添加失败', 'failed');
+            }
+            return $this->resultArray();
+        }
     }
 
     /**
@@ -42,16 +70,17 @@ class User extends Common
      * @param  \think\Request $request
      * @param  int $id
      * @return \think\Response
+     * @auther guozhen
      */
     public function update($id)
     {
-        if($this->request->isPut()){
-            $rule=[
-                ["user_name","require","请输入用户名"],
-                ["pwd","require","请输入密码"],
+        if ($this->request->isPut()) {
+            $rule = [
+                ["user_name", "require", "请输入用户名"],
+                ["pwd", "require", "请输入密码"],
                 []
             ];
-            $data=\app\common\model\User::create();
+            $data = \app\common\model\User::create();
 
         }
 
@@ -68,13 +97,12 @@ class User extends Common
     public function delete($id)
     {
         if ($this->request->isDelete()) {
-//            return 1111;die;
             if ($id == 1) {
                 return $this->resultArray('系统管理员不允许删除', 'failed');
             }
             $user = \app\common\model\User::get($id);
             if (!$user->delete()) {
-                return $this->resultArray('删除失败','failed');
+                return $this->resultArray('删除失败', 'failed');
             }
             return $this->resultArray('删除成功');
 
