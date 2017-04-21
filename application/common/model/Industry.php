a9d7f8a59f47438a3890645fe8580e3af0a35090
2301887641 <2301887641@qq.com>
2017-04-21 15:17:52 +0800

    Merge branch 'dev' into jingzheng
    # Conflicts:
    #	application/common/controller/User.php

--- application/common/model/Industry.php
+++ application/common/model/Industry.php
@@ -6,9 +6,24 @@
  * Time: 11:35
  */
 namespace app\common\model;
+
+use think\Model;
+
 class Industry extends Model{
     protected $rule = [
         ['name', 'require', '公司名必须'],
         ['detail', 'require', '详细必须'],
     ];
+    public function getIndustry($limit, $rows)
+    {
+        $count = $this->count();
+        $data = $this->limit($limit, $rows)->order("id", "desc")->field("id,name,detail,create_time,update_time")->select();
+        return [
+            "total" => $count,
+            "rows" => $data
+        ];
+    }
+
+
+
 }
