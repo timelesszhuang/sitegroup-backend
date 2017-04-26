471e89c47c197d5bc67e26270d291d6a11b88344
2301887641 <sgz19861126>
2017-04-26 14:29:10 +0800

    save

---
+++ application/admin/model/Keyword.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiangbi
+ * Date: 17-4-26
+ * Time: 下午2:25
+ */
+
+namespace app\model\Keyword;
+
+use think\Model;
+
+class Keyword extends Model
+{
+    /**
+     * 根据tag获取数据
+     * @param $tag
+     * @return false|\PDOStatement|string|\think\Collection
+     */
+    public function getKeyword($tag)
+    {
+        $where["tag"]=$tag;
+        $data=$this->where($where)->select();
+        return $data;
+    }
+
+
+
+}
