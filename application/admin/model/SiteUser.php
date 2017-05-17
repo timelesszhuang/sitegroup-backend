56fe331b2029734d3fd50f7aa15fdd9760c46eb9
2301887641 <sgz19861126>
2017-05-17 09:31:29 +0800

    is_on

--- application/admin/model/SiteUser.php
+++ application/admin/model/SiteUser.php
@@ -7,9 +7,7 @@ use think\Model;
 class SiteUser extends Model
 {
     //只读字段
-    protected $readonly=["node_id"];
-
-    Site
+    protected $readonly = ["node_id"];
 
 
 
@@ -24,11 +22,24 @@ class SiteUser extends Model
     public function getAll($limit, $rows, $where)
     {
         $count = $this->where($where)->count();
-        $data = $this->limit($limit, $rows)->where($where)->field('update_time',true)->order('id','desc')->select();
+        $data = $this->limit($limit, $rows)->where($where)->field('update_time', true)->order('id', 'desc')->select();
         return [
             "total" => $count,
             "rows" => $data
         ];
 
+    }
+
+    /**
+     * 格式化数据返回
+     * @param $is_on
+     * @return string
+     */
+    public function getIsOnAttr($is_on)
+    {
+        if ($is_on == 0) {
+            return "禁用";
+        }
+        return "启用";
     }
 }
