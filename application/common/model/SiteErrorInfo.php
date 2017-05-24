7766ed2116ebd312ec56ff352c668cf96a879454
2301887641 <sgz19861126>
2017-05-24 17:38:10 +0800

    save

--- application/common/model/SiteErrorInfo.php
+++ application/common/model/SiteErrorInfo.php
@@ -17,7 +17,7 @@ class SiteErrorInfo extends Model
     public function getAll($limit,$rows,$where)
     {
         $count = $this->where($where)->count();
-        $data = $this->limit($limit, $rows)->where($where)->field("msg,operator,site_name")->select();
+        $data = $this->limit($limit, $rows)->where($where)->field("id,msg,operator,site_name")->select();
         return [
             "total" => $count,
             "rows" => $data
