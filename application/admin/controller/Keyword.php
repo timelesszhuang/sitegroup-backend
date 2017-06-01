0eb07f0115eba3396013a967167afde3f170f12c
2301887641 <sgz19861126>
2017-06-01 09:46:27 +0800

    find

--- application/admin/controller/Keyword.php
+++ application/admin/controller/Keyword.php
@@ -138,7 +138,7 @@ class Keyword extends Common
         $model = new \app\admin\model\Keyword();
         $file_info=$this->getKeywordInfo($post["path"], $post["id"],$model);
         while ($key = fgets($file_info["file"])) {
-            $getkey = $model->where(["name" => $key])->select();
+            $getkey = $model->where(["name" => $key])->find();
             dump($getkey);die;
             if (!empty($getkey) && ($getkey->parent_id==$post["id"])) {
                 continue;
