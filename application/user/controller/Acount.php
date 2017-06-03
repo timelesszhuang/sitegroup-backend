2750e5c58134ee1179fdedf28831a75c808544eb
yangjingzheng <1318911846@qq.com>
2017-06-03 09:30:14 +0800

    save

--- application/user/controller/Acount.php
+++ application/user/controller/Acount.php
@@ -26,7 +26,7 @@ class Acount extends Common
         $arrcount = (new BrowseRecord())->where($where)->count();
         $temp=[];
         foreach ($arr as $k=>$v){
-            $temp[]=[$v['engine'],floatval($v['keyCount']/$arrcount*100,2)];
+            $temp[]=[$v['engine'],round($v['keyCount']/$arrcount*100,2)];
         }
         return $this->resultArray('','',$temp);
 
