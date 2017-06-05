2a5215549bcc26a75addd16830ea8647525ab606
yangjingzheng <1318911846@qq.com>
2017-06-05 11:37:54 +0800

    统计

--- application/admin/controller/Count.php
+++ application/admin/controller/Count.php
@@ -17,16 +17,16 @@ class Count extends Common
     public function index()
     {
 //      $node_id=$this->getSiteSession('login_site');
-        $param=$this->request->get();
-        $starttime = 0;
-        $stoptime = time();
-        if(!$param){
-            list($start_time,$stop_time)=$param['time'];
-            $starttime = strtotime($start_time);
-            $stoptime=strtotime($stop_time);
-        }
+//        $param=$this->request->get();
+//        $starttime = 0;
+//        $stoptime = time();
+//        if(!$param){
+//            list($start_time,$stop_time)=$param['time'];
+//            $starttime = strtotime($start_time);
+//            $stoptime=strtotime($stop_time);
+//        }
         $where = [
-            'create_time'=>['between',[$starttime,$stoptime]],
+//            'create_time'=>['between',[$starttime,$stoptime]],
             'node_id'=>2,
             'site_id'=>1
         ];
