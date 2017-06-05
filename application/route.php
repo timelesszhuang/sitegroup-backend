83b1bc9fb7d21a85e2b1125a5806d963a04e4efd
yangjingzheng <1318911846@qq.com>
2017-06-05 09:29:12 +0800

    统计

--- application/route.php
+++ application/route.php
@@ -115,6 +115,7 @@ Route::resource('links','admin/Links');
 Route::get('links/getLinks','admin/Links/getLinks');
 //节点统计
 Route::resource("count",'admin/Count');
+Route::resource("countkeywork",'admin/CountKeywork');
 //小站点相关--------------------------------------------------------
 //小站点登录后的首页操作
 Route::post('user/siteInfo','user/index/siteInfo');
