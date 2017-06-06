cd552b00b48f9cc4aef107c16d6656a5812c0e74
yangjingzheng <1318911846@qq.com>
2017-06-06 15:51:43 +0800

    统计

--- application/admin/controller/Count.php
+++ application/admin/controller/Count.php
@@ -18,7 +18,7 @@ class Count extends Common
     {
 //      $node_id=$this->getSiteSession('login_site');
         $param=$this->request->get();
-            dump($param);die;
+//            dump($param);die;
         $user=$this->getSessionUser();
         $starttime = 0;
         $stoptime = time();
