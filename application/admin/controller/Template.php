33f91b15eca091ca63050761bdf31c5c3dafef42
2301887641 <2301887641@qq.com>
2017-06-29 10:27:00 +0800

    修改模板

--- application/admin/controller/Template.php
+++ application/admin/controller/Template.php
@@ -5,10 +5,10 @@ namespace app\admin\controller;
 use app\common\controller\Common;
 use think\Request;
 use think\Validate;
-
+use app\common\traits\Obtrait;
 class Template extends Common
 {
-
+    use Obtrait;
     static $templatepath = 'upload/template/';
 
     /**
@@ -30,26 +30,7 @@ class Template extends Common
         return $this->resultArray('', '', $data);
     }
 
-    /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-
-    }
 
     /**
      * 显示指定的资源
@@ -60,17 +41,6 @@ class Template extends Common
     public function read($id)
     {
         return $this->getread((new \app\admin\model\Template), $id);
-    }
-
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
@@ -170,5 +140,89 @@ class Template extends Common
     {
         $field="id,name as text";
         return $this->getList((new \app\admin\model\Template),$field);
+    }
+
+    /**
+     * 获取站点模板列表
+     * @param $site_id
+     * @return array
+     */
+    public function filelist($site_id)
+    {
+        $url="templatelist";
+        $site=\app\admin\model\Site::get($site_id);
+        if($site){
+            $siteData=$this->curl_get($site->url."/index.php/$url?site_id=".$site_id);
+            $data=json_decode($siteData,true);
+            return $this->resultArray($data['msg'],'',$data["filelist"]);
+        }
+        return $this->resultArray('当前网站未获取到!','failed');
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function templateRead($site_id,$name)
+    {
+        $url="templateread";
+        $site=\app\admin\model\Site::get($site_id);
+        if($site){
+            $siteData=$this->curl_get($site->url."/index.php/$url?site_id=".$site_id."&filename=".$name);
+            $data=json_decode($siteData,true);
+            return $this->resultArray($data['msg'],'',["content"=>$data["content"],"filename"=>$data["filename"]]);
+        }
+        return $this->resultArray('当前网站未获取到!','failed');
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save($site_id,$name)
+    {
+        $request=Request::instance();
+        $content=$request->post("content");
+        $url="templateupdate";
+        $site=\app\admin\model\Site::get($site_id);
+        if($site) {
+            $send = [
+                "site_id" => $site_id,
+                "filename" => $name,
+                "content" => $content
+            ];
+            $siteData=$this->curl_post($site->url."/index.php/".$url,$send);
+            $data=json_decode($siteData,true);
+            return $this->resultArray($data['msg'],$data["status"]);
+        }
+        return $this->resultArray('当前网站未获取到!','failed');
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function readFile($site_id,$name)
+    {
+        $request=Request::instance();
+        $content=$request->post("content");
+        $url="templateadd";
+        $site=\app\admin\model\Site::get($site_id);
+        if($site) {
+            $send = [
+                "site_id" => $site_id,
+                "filename" => $name,
+                "content" => $content
+            ];
+            $siteData=$this->curl_post($site->url."/index.php/".$url,$send);
+            $data=json_decode($siteData,true);
+            return $this->resultArray($data['msg'],$data["status"]);
+        }
+        return $this->resultArray('当前网站未获取到!','failed');
     }
 }
--- application/route.php
+++ application/route.php
@@ -147,13 +147,13 @@ Route::resource('Rejection','admin/Rejection');
 //crontab任务,每天定时执行更新所有网站的静态页面
 Route::get('crontab','admin/CrontabTask/index');
 //模板管理 获取对应site_id的信息
-Route::get("mobileTemplate/:site_id","admin/MobileTemplate/index");
+Route::get("templateList/:site_id","admin/template/filelist");
 // 模板管理 读取模板
-Route::get("templateRead/:site_id/:name","admin/MobileTemplate/templateRead");
+Route::get("templateRead/:site_id/:name","admin/template/templateRead");
 // 模板管理 修改模板
-Route::post("templateSave/:site_id/:name","admin/MobileTemplate/save");
+Route::post("templateSave/:site_id/:name","admin/template/save");
 //模板管理 新加模板
-Route::post("templateAdd/:site_id/:name","admin/MobileTemplate/read");
+Route::post("templateAdd/:site_id/:name","admin/template/readFile");
 
 
 
