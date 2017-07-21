4547ccb6bc4e28d952fd0f1a2ff8338c2f0e1c08
2301887641 <2301887641@qq.com>
2017-07-21 10:19:55 +0800

    delete template

---
+++ application/admin/controller/WeixinKeyword.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+use think\Request;
+use \app\admin\model\WeixinKeyword as Scrapy;
+class WeixinKeyword extends Controller
+{
+    protected $conn='';
+    /**
+     * 初始化操作
+     */
+    public function _initialize()
+    {
+        $this->conn=new Scrapy();
+    }
+    /**
+     * 获取关键字
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        return $this->conn->getKeyword();
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
