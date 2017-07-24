08f3d115ded5c763fa879fdb57c6b69c229c7c05
2301887641 <2301887641@qq.com>
2017-07-24 14:27:22 +0800

    keyword

--- application/admin/controller/WeixinArticle.php
+++ application/admin/controller/WeixinArticle.php
@@ -28,9 +28,13 @@ class WeixinArticle extends Common
     {
         $request = $this->getLimit();
         $title= $this->request->get('title');
+        $keyword= $this->request->get('keyword');
         $where = [];
         if (!empty($title)) {
             $where["title"] = ["like", "%$title%"];
+        }
+        if(!empty($keyword)){
+            $where["keyword"]=$keyword;
         }
         $data = $this->conn->getArticle($request["limit"], $request["rows"], $where);
         return $this->resultArray('', '', $data);
