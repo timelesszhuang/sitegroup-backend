c1115105a7ed7389ac22f8c6028b59bbadcbd309
yangjingzheng <1318911846@qq.com>
2017-08-09 13:54:37 +0800

    edit

--- application/admin/model/Menu.php
+++ application/admin/model/Menu.php
@@ -24,7 +24,7 @@ class Menu extends Model
     public function getMenu($limit, $rows, $where)
     {
         $count = $this->where($where)->count();
-        $data = $this->limit($limit, $rows)->where($where)->select();
+        $data = $this->limit($limit, $rows)->where($where)->order('id desc')->select();
         return [
             "total" => $count,
             "rows" => $data
