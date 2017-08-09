dc78476ed584a281daa73eea994bb8a2b24fca4a
yangjingzheng <1318911846@qq.com>
2017-08-09 14:19:40 +0800

    文章分类标签修改

--- application/admin/controller/Articletype.php
+++ application/admin/controller/Articletype.php
@@ -119,6 +119,9 @@ class Articletype extends Common
     {
         $where = [];
         $data = (new \app\admin\model\Articletype())->getArttype($where);
+        foreach ($data as$k=>$v){
+            $v['name'] = $v['name'].'['.$v['tag'].']';
+        }
         return $this->resultArray('', '', $data);
     }
 
