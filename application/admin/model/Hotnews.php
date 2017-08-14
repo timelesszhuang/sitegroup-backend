dfcd002db129da74d6c097c1a55e66e357bf95ad
yangjingzheng <1318911846@qq.com>
2017-08-14 08:52:53 +0800

    zuixin

--- application/admin/model/Hotnews.php
+++ application/admin/model/Hotnews.php
@@ -5,7 +5,7 @@ namespace app\admin\model;
 use think\Db;
 use think\Model;
 
-class HotNews extends Model
+class Hotnews extends Model
 {
     // 设置当前模型对应的完整数据表名称
     protected $table = 'sc_hotnews';
