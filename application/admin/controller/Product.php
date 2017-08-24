1136cfcd516053197c387eee7dd84f679a47eaaa
2301887641 <2301887641@qq.com>
2017-08-24 14:54:15 +0800

    产品

---
+++ application/admin/controller/Product.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+use think\Request;
+
+class Product extends Controller
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        $request = $this->getLimit();
+        $name = $this->request->get('name');
+        $where = [];
+        if (!empty($name)) {
+            $where["name"] = ["like", "%$name%"];
+        }
+        $user = $this->getSessionUser();
+        $where["node_id"] = $user["user_node_id"];
+        $data = (new produ())->getAll($request["limit"], $request["rows"], $where);
+        return $this->resultArray('', '', $data);
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        $post = $request->post();
+        $rule = [
+            ["name", "require", "请输入产品名称"],
+            ["summary", "require", "请输入摘要"],
+            ["detail", "require", "请输入详情"],
+            ["image",'require',"请上传图片"]
+        ];
+        $validate = new Validate($rule);
+        if (!$validate->check($post)) {
+            return $this->resultArray($validate->getError(), 'failed');
+        }
+        $user = $this->getSessionUser();
+        $post["node_id"] = $user["user_node_id"];
+        $model = new \app\admin\model\Template();
+        $model->save($post);
+        if ($model->id) {
+            return $this->resultArray("添加成功");
+        }
+        return $this->resultArray('添加失败', 'failed');
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+
+    /**
+     * 上传图片文件
+     * @return array
+     */
+    public function uploadImage()
+    {
+        $file = request()->file('file_name');
+        $info = $file->move(ROOT_PATH . 'public/static');
+        if ($info) {
+            return $this->resultArray('上传成功', '', $info->getSaveName());
+        } else {
+            // 上传失败获取错误信息
+            return $this->resultArray('上传失败', 'failed', $info->getError());
+        }
+    }
+}
