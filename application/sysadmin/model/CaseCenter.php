1e1c83b26ee418c9eeb7403ee43d88a9e07629db
2301887641 <2301887641@qq.com>
2017-09-06 17:36:30 +0800

    营销中心

---
+++ application/sysadmin/model/CaseCenter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\sysadmin\model;
+
+use think\Model;
+
+class CaseCenter extends Model
+{
+    /**
+     * 分页
+     * @param $limit
+     * @param $rows
+     * @return array
+     * @author jingzheng
+     */
+    public function getList($limit, $rows,$where=0)
+    {
+        $count=$this->where($where)->count();
+        $data = $this->limit($limit, $rows)->order("id", "desc")->field("update_time",true)->where($where)->select();
+        return [
+            "total" => $count,
+            "rows" => $data
+        ];
+    }
+}
