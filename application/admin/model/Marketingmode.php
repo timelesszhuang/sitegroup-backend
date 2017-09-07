529b2e1566b82a095a58ef18d5115b45abbae7e7
2301887641 <2301887641@qq.com>
2017-09-07 11:46:07 +0800

    营销改成图片格式

--- application/admin/model/Marketingmode.php
+++ application/admin/model/Marketingmode.php
@@ -16,7 +16,7 @@ class Marketingmode extends Model
     public function getList($limit, $rows,$where=0)
     {
         $count=$this->where($where)->count();
-        $data = $this->limit($limit, $rows)->order("id", "desc")->field("update_time",true)->where($where)->select();
+        $data = $this->limit($limit, $rows)->order("id", "desc")->field("id,title,img")->where($where)->select();
         return [
             "total" => $count,
             "rows" => $data
