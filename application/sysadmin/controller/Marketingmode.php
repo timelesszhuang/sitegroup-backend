807415caf870355f49388c3928c2c55005c5fcb6
2301887641 <2301887641@qq.com>
2017-09-07 11:05:39 +0800

    营销模式所洛特

--- application/sysadmin/controller/Marketingmode.php
+++ application/sysadmin/controller/Marketingmode.php
@@ -62,7 +62,8 @@ class Marketingmode extends Common
             ["content", "require", "请输入内容"],
             ["industry_id", "require", "请选择行业分类"],
             ["industry_name","require","请选择行业分类"],
-            ["keyword","require","请填写关键词"]
+            ["keyword","require","请填写关键词"],
+            ["img","require","请上传缩略图"]
         ];
         $validate = new Validate($rule);
         $data = $request->post();
@@ -138,5 +139,21 @@ class Marketingmode extends Common
             return $this->resultArray('删除失败', 'failed');
         }
         return $this->resultArray('删除成功');
+    }
+
+    /**
+     * 上传图片文件
+     * @return array
+     */
+    public function uploadImage()
+    {
+        $file = request()->file('file_name');
+        $info = $file->move(ROOT_PATH . 'public/upload/marketingmode');
+        if ($info) {
+            return $this->resultArray('上传成功', '', "upload/marketingmode/".$info->getSaveName());
+        } else {
+            // 上传失败获取错误信息
+            return $this->resultArray('上传失败', 'failed', $info->getError());
+        }
     }
 }
