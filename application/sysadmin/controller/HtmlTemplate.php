3b80dd0c0c6d9c019ddd8dc266c38996ea2e229d
2301887641 <2301887641@qq.com>
2017-09-15 09:55:57 +0800

    事件营销活动

---
+++ application/sysadmin/controller/HtmlTemplate.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace app\sysadmin\controller;
+
+use think\Controller;
+use think\Request;
+
+class HtmlTemplate extends Controller
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        $rule = [
+            ["img", "require", "请上传图片"],
+            ['path', 'require', '请上传模板'],
+        ];
+
+        $data = $this->request->post();
+        $validate = new Validate($rule);
+        if (!$validate->check($data)) {
+            return $this->resultArray($validate->getError(), 'failed');
+        }
+        $data["startday"]=date("Y-m-d",strtotime($data["time"][0]));
+        $data["endday"]=date("Y-m-d",strtotime($data["time"][1]));
+        unset($data["time"]);
+        if (!mark::create($data)) {
+            return $this->resultArray('添加失败', 'failed');
+        }
+        return $this->resultArray('添加成功');
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+
+    /**
+     * 上传html5模板
+     * @return array
+     */
+    public function uploadTemplate()
+    {
+        $file = request()->file('file_name');
+        $info = $file->move(ROOT_PATH . 'public/upload/htmlTemplate');
+        if ($info) {
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            return $this->resultArray('上传成功', '', $info->getSaveName());
+        } else {
+            // 上传失败获取错误信息
+            return $this->resultArray('上传失败', 'failed', $info->getError());
+        }
+    }
+
+    /**
+     * 上传html5模板缩略图
+     * @return array
+     */
+    public function uploadTemplateImg()
+    {
+        $file = request()->file('img_name');
+        $info = $file->move(ROOT_PATH . 'public/upload/htmlTemplateImg');
+        if ($info) {
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            return $this->resultArray('上传成功', '', $info->getSaveName());
+        } else {
+            // 上传失败获取错误信息
+            return $this->resultArray('上传失败', 'failed', $info->getError());
+        }
+    }
+}
