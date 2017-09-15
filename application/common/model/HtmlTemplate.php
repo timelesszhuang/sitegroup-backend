0bc630d67126b878b12d9d2ffa9cfad47e43b1c1
2301887641 <2301887641@qq.com>
2017-09-15 17:03:28 +0800

    事件营销活动

--- application/common/model/HtmlTemplate.php
+++ application/common/model/HtmlTemplate.php
@@ -17,8 +17,19 @@ class HtmlTemplate extends Model
             if (!empty($template->path)) {
                 $zip=new \ZipArchive();
                 $root=dirname(THINK_PATH);
+                $status='';
                 if($zip->open($root."/public/".$template->path)==true){
                     $zip->extractTo($root.'/public/upload/unHtmlTemplate');
+                    for($i = 0; $i < $zip->numFiles; $i++) {
+                        $filename = $zip->getNameIndex($i);
+                        $fileinfo = pathinfo($filename);
+                        if(isset($fileinfo["dirname"])){
+                            if($fileinfo["dirname"]!="." && strstr($fileinfo["dirname"],"/")===false){
+                                $template->generated_path="unHtmlTemplate/".$fileinfo["dirname"]."/index.html";
+                            }
+                        }
+
+                    }
                     $zip->close();
                 }
             }
