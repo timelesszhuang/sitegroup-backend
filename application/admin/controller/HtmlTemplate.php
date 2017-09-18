f8cd009f83ffc07d76f2703df8b38d06f171440f
2301887641 <2301887641@qq.com>
2017-09-18 17:05:59 +0800

    事件营销活动

--- application/admin/controller/HtmlTemplate.php
+++ application/admin/controller/HtmlTemplate.php
@@ -49,7 +49,7 @@ class HtmlTemplate extends Common
    {
         $request=Request::instance();
         $callback=$request->get('callback');
-        $data=(new Html)->where(["id"=>$id])->field(["generated_path"])->find();
+        $data=(new Html)->where(["id"=>$id])->field(["generated_path,template_name"])->find();
         $fdata["title"]=$data["template_name"];
         $fdata["data"]="error";
         if(is_file(ROOT_PATH."public/upload/".$data["generated_path"]."/index.html")){
