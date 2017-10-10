575168360bd19b130d4cb1f405e15572f884462b
2301887641 <2301887641@qq.com>
2017-10-10 14:14:02 +0800

    事件营销活动

--- application/sysadmin/controller/Media.php
+++ application/sysadmin/controller/Media.php
@@ -73,7 +73,7 @@ class Media extends Common
      */
     public function read($id)
     {
-        return $this->getread(new Me(), $id));
+        return $this->getread((new Me()), $id);
     }
 
     /**
