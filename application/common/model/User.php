02fae90a429b338e40deddc9f52eb25e6f9587b5
2301887641 <2301887641@qq.com>
2017-10-13 15:44:02 +0800

    事件营销活动

--- application/common/model/User.php
+++ application/common/model/User.php
@@ -48,13 +48,15 @@ class User extends Model
         if (md5($pwd . $username) != $user_info_arr["pwd"]) {
             return ["用户名或密码错误", "failed", ''];
         }
-        // 查询node_id是否被禁用 如果被禁同样禁止登录
-        $node_info=Node::where(["id"=>$user_info_arr["node_id"]])->find();
-        if(empty($node_info)){
-            return ["当前用户没有节点后台!!", "failed",''];
-        }
-        if($node_info["status"]=="off"){
-            return ["当前节点后台禁止登录!!", "failed",''];
+        if($user_info_arr["id"]!=1) {
+            // 查询node_id是否被禁用 如果被禁同样禁止登录
+            $node_info = Node::where(["id" => $user_info_arr["node_id"]])->find();
+            if (empty($node_info)) {
+                return ["当前用户没有节点后台!!", "failed", ''];
+            }
+            if ($node_info["status"] == "off") {
+                return ["当前节点后台禁止登录!!", "failed", ''];
+            }
         }
         unset($user_info["pwd"]);
         //获取私钥
