b03e8a70314c900d0346944fb00bcfdfefabbaf7
2301887641 <2301887641@qq.com>
2017-10-17 13:45:52 +0800

    事件营销活动

--- application/admin/controller/Company.php
+++ application/admin/controller/Company.php
@@ -45,28 +45,7 @@ class Company extends Common
      */
     public function save(Request $request)
     {
-//        $rule=[
-//            ["name","require","请输入机构名称"],
-//            ["industry_id","require","请输入行业名称"],
-//            ["industry_name","require","请输入行业名称"],
-//            ["tax_registration_number","require","请输入税务登记号"],
-//            ["business_license","require","请上传营业执照"],
-//            ["artificialperson","require","请输入法人名称"],
-//            ["artificialperson_id","require","请输入法人身份证"],
-//            ["sale_manage","require","请输入营销系统负责人"],
-//            ["sale_manage_phone","require","请输入营销系统负责人电话"],
-//            ["manbusiness","require","请输入公司主营业务"]
-//        ];
-//        $validate=new Validate($rule);
-//        $post=$request->post();
-//        if(!$validate->check($post)){
-//            return $this->resultArray($validate->getError(),"failed");
-//        }
-//        $com=new Com($post);
-//        if(!$com->allowField(true)->save()){
-//            return $this->resultArray("添加失败!","failed");
-//        }
-//        return $this->resultArray("添加成功!");
+
     }
 
     /**
