ccbd6202053f1e5b6c3a6ec572dd98b7c257f8f8
2301887641 <2301887641@qq.com>
2017-10-17 11:16:33 +0800

    事件营销活动

--- application/admin/controller/Company.php
+++ application/admin/controller/Company.php
@@ -164,4 +164,20 @@ class Company extends Common
         return $this->resultArray('上传失败',"failed");
     }
 
+    /**
+     * 上传商标
+     * @return array
+     */
+    public function uploadTrademark()
+    {
+        $request=Request::instance();
+        $trademark=$request->file("trademark");
+        $path='/upload/company/trademark/';
+        $info=$trademark->move(ROOT_PATH.'public'.$path);
+        if($info){
+            return $this->resultArray('上传成功!','',$path.$info->getSaveName());
+        }
+        return $this->resultArray('上传失败!',"failed");
+    }
+
 }
