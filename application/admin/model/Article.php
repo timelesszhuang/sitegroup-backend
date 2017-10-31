2e2f67e6f851e2cd648283de2767e5e720665a41
timelessz <834916321@qq.com>
2017-10-31 17:18:14 +0800

    发送文件到指定 远程服务器

--- application/admin/model/Article.php
+++ application/admin/model/Article.php
@@ -44,7 +44,7 @@ class Article extends Model
                     //有上传缩略图 一定是oss的
                     //拼接缩略图名称
                     //没有图片的
-                    if (!$article->id) {
+                    if (!array_key_exists('id', $article)) {
                         //添加的时候才需要操作
                         $imgname = md5(uniqid(rand(), true));
                         $type = pathinfo(parse_url($article->thumbnails)['path'])['extension'];
