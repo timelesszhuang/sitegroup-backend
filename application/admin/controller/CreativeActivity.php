bdb549e0f20b44602556e460ee6f0acbac4e82d2
2301887641 <2301887641@qq.com>
2017-11-09 15:27:47 +0800

    事件营销活动

--- application/admin/controller/CreativeActivity.php
+++ application/admin/controller/CreativeActivity.php
@@ -54,8 +54,8 @@ class CreativeActivity extends Common
     {
         $rule=[
             ["title","require","请输入标题"],
-            ["oss_img_src","require",""],
-            ["img_name","require",""],
+            ["oss_img_src","require","请传递封面"],
+            ["img_name","require","请上传图片名"],
             ["imgser","require",""],
             ["url","require",""],
             ["keywords","require",""]
@@ -130,11 +130,13 @@ class CreativeActivity extends Common
            ["oss_img_src","require","请先上传图片"],
            ["url","require","请输入外部链接"]
        ];
-       $validate=new Validate();
-       $data=$request->input();
+       $validate=new Validate($rule);
+       $data=$request->post();
        if(!$validate->check($data)){
            return $this->resultArray($validate->getError());
        }
+        $user = $this->getSessionUser();
+        $data["node_id"] = $user["user_node_id"];
        if(creative::create($data)){
            return $this->resultArray("添加成功");
        }
