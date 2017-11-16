f223c5ec122c4587d2d59f98e6cb02fd02b8db14
yangjingzheng <1318911846@qq.com>
2017-11-16 18:12:26 +0800

    oss

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -265,7 +265,7 @@ class Article extends Common
                     'site_name' => $vv['site_name'],
                 ];
             }
-            if ($showhtml) {
+            if (!empty($showhtml)) {
                 return $this->resultArray('', '', $showhtml);
             } else {
                 return $this->resultArray('当前无法预览', 'failed');
