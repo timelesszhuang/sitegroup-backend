438149dda94f1681c217fa2d6077ccd7cc52283c
sunjingyang <linshuboy@qq.com>
2017-12-15 09:30:40 +0800

    修改导出数据字段

--- application/admin/model/Article.php
+++ application/admin/model/Article.php
@@ -109,7 +109,7 @@ class Article extends Model
     public function getArticle($limit, $rows, $where = 0)
     {
         $count = $this->where($where)->count();
-        $data = $this->limit($limit, $rows)->where($where)->field('content,summary,update_time,readcount', true)->order('id desc')->select();
+        $data = $this->limit($limit, $rows)->where($where)->field('content,summary,update_time,readcount,title_color', true)->order('id desc')->select();
         return [
             "total" => $count,
             "rows" => $data
