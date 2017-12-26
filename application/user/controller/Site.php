9daf7709e2cf5b95fb899492f941df9edfbb011f
sunjingyang <linshuboy@qq.com>
2017-12-26 10:58:36 +0800

    修改小站点联系方式字段

--- application/user/controller/Site.php
+++ application/user/controller/Site.php
@@ -47,13 +47,7 @@ class Site extends Common
         if(empty($site_id)){
             return $this->resultArray("获取站点错误","failed");
         }
-        $rule = [
-        ];
-        $validate = new Validate($rule);
         $data = \request()->post();
-        if (!$validate->check($data)) {
-            return $this->resultArray($validate->getError(), "failed");
-        }
         $id = $data['id'];
         unset($data['id']);
         if(!\app\admin\model\Site::update($data,["id"=>$site_id])){
