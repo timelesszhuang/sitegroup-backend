66b43cf8be58746be6205bcdba2d7482443e5aa0
timelessz <834916321@qq.com>
2017-12-27 17:00:55 +0800

    发送文件到指定 远程服务器

--- application/admin/model/Article.php
+++ application/admin/model/Article.php
@@ -44,6 +44,7 @@ class Article extends Model
                     //有上传缩略图 一定是oss的
                     //拼接缩略图名称
                     //没有图片的
+                    var_dump(array_key_exists('id',$article));
                     print_r($article);
                     exit;
                     if (!array_key_exists('id', $article)) {
