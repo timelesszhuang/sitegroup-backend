61bcbf4defcf6d0e35015d997700fcd59d37b35c
sunjingyang <linshuboy@qq.com>
2017-12-27 14:38:57 +0800

    添加分类

--- application/admin/controller/Articletype.php
+++ application/admin/controller/Articletype.php
@@ -64,6 +64,7 @@ class Articletype extends Common
         $rule = [
             ["name", "require", "请输入文章分类名"],
             ["detail", "require", "请输入详情"],
+            ["tag_id", "require", "请输入或选择分类"],
             ["alias", "require", "请输入此分类的英文名"]
         ];
         $validate = new Validate($rule);
@@ -73,15 +74,40 @@ class Articletype extends Common
         $where['node_id'] = $user['user_node_id'];
         $where['alias'] = $data['alias'];
         $typedata = (new \app\admin\model\Articletype())->where($where)->select();
-        if ($typedata) {
-            return $this->resultArray("此分类的英文名重复,请重试", "failed");
+        Db::startTrans();
+        try{
+            if ($typedata) {
+                exception("此分类的英文名重复,请重试");
+            }
+            if(isset($data['tag_name'])&&$data['tag_name']){
+                $where1['node_id'] = $user['user_node_id'];
+                $where1['tag'] = $data['tag_name'];
+                $Type_Tag = new Type_Tag;
+                $typetag = $Type_Tag->where($where1)->find();
+                if($typetag){
+                    $data['tag_id']=$typetag['id'];
+                }else{
+                    $data_tag['tag'] = $data['tag_name'];
+                    $data_tag['node_id'] = $user['user_node_id'];
+                    if (!$Type_Tag::create($data_tag)) {
+                        exception("标签创建失败");
+                    }
+                    $data['tag_id']=$Type_Tag->getLastInsID();
+                }
+                unset($data['tag_name']);
+            }
+            if (!$validate->check($data)) {
+                return $this->resultArray($validate->getError(), "failed");
+            }
+            if (!\app\admin\model\Articletype::create($data)) {
+                exception("类型创建失败");
+            }
+            Db::commit();
+        }catch (\Exception $e){
+            Db::rollback();
+            return $this->resultArray($e->getMessage(), "failed");
         }
-        if (!$validate->check($data)) {
-            return $this->resultArray($validate->getError(), "failed");
-        }
-        if (!\app\admin\model\Articletype::create($data)) {
-            return $this->resultArray("添加失败", "failed");
-        }
+
         return $this->resultArray("添加成功");
     }
 
