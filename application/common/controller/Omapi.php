dfe252cebfecf7517d57a98ccbfcf69a260d1047
linshuboy <linshuboy@qq.com>
2018-01-24 14:48:25 +0800

    解析来电xml数据添加到数据库

--- application/common/controller/Omapi.php
+++ application/common/controller/Omapi.php
@@ -7,6 +7,7 @@ use think\Model;
 use app\sysadmin\model\Node;
 use app\common\model\VoiceCdr;
 use think\Request;
+use think\cache;
 
 class Omapi extends Controller
 {
@@ -79,7 +80,9 @@ class Omapi extends Controller
             $data[$name]['val'] = empty($val) ? '' : $val;
         }
         //获取   分机号码=>array(userid,flag)  跨分组调用模块
-        $extnum_nodeid_arr = $this->get_extnum_nodeid_arr();
+        $extnum_nodeid_arr = Cache::remember('extnum_nodeid_arr',function(){
+            return  $this->get_extnum_nodeid_arr();
+        });
         //通话的唯一标识
         $cdr_data['callid'] = $data['callid']['val'];
         //$cdr_data['visitor'] = $data['visitor']['attr']['id'];
