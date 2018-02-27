96f47fff0898c9ebc1275ee03aa937690580aa08
yangjingzheng <1318911846@qq.com>
2018-02-27 11:11:31 +0800

    api

--- application/common/controller/Tdk.php
+++ application/common/controller/Tdk.php
@@ -18,7 +18,17 @@ class Tdk extends Common
      */
     public function save($id)
     {
-        return (new PageInfo)->update($id);
+        $rule = [
+            ["page_id", "require", "请输入页面id"],
+        ];
+        $validate = new Validate($rule);
+        $request = Request::instance();
+        $data = $request->put();
+        if (!$validate->check($data)) {
+            return $this->resultArray("failed",$validate->getError() );
+        }
+        return $this->publicUpdate((new SitePageinfo), $data, $id);
+
     }
 
     /**
@@ -29,8 +39,8 @@ class Tdk extends Common
     public function search($id)
     {
         $request = $this->getLimit();
-        $user = $this->getSessionUser();
-        $where["node_id"] = $user["user_node_id"];
+        $user_info = $this->getSessionUserInfo();
+        $where["node_id"] = $user_info["node_id"];
         $where["site_id"] = $id;
         $data = (new SitePageinfo)->getAll($request["limit"], $request["rows"], $where);
         return $this->resultArray('', '', $data);
@@ -75,12 +85,12 @@ class Tdk extends Common
     {
         $data = $this->request->post();
         if ($data['akeyword_id'] == 0) {
-            return $this->resultArray('首页关键词不能修改', 'failed');
+            return $this->resultArray( 'failed','首页关键词不能修改');
         }
         if (SitePageinfo::update($data)) {
             return $this->resultArray('修改成功');
         }else{
-            return $this->resultArray('修改失败','failed');
+            return $this->resultArray('failed','修改失败');
         }
         return $this->resultArray('修改成功');
     }
