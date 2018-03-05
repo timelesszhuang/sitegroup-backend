13bb5e1aedaf1d03cb21a92522771a8efce685f8
linshuboy <linshuboy@qq.com>
2018-03-05 16:02:23 +0800

    。

--- application/common/controller/Site.php
+++ application/common/controller/Site.php
@@ -387,7 +387,7 @@ class Site extends Common
             $NewUrl = '';
             $msg = '';
             switch ($name) {
-                case "aKeyGeneration":
+                case "oneKeyGenerate":
                     $msg = "正在一键生成...";
                     $NewUrl = $url . "/allstatic";
                     break;
