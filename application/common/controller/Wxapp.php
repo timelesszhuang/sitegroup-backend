a2108077e05dcb7db894dbbf6e5e4c9762613cde
linshuboy <linshuboy@qq.com>
2018-03-15 17:15:56 +0800

    wxappapi

--- application/common/controller/Wxapp.php
+++ application/common/controller/Wxapp.php
@@ -13,6 +13,8 @@ use think\Request;
 use think\Session;
 use think\Cookie;
 use app\common\model\User;
+use app\common\model\Node;
+use think\Validate;
 
 class Wxapp extends Common
 {
@@ -39,8 +41,8 @@ class Wxapp extends Common
             if(array_key_exists('errcode',$return)){
                 Common::processException(/*$return['errmsg']*/'参数错误');
             }
-            Session::set('openid',$return['openid']);
-            Session::set('session_key',$return['session_key']);
+            Session::set('openid',$return['openid'],'wx');
+            Session::set('session_key',$return['session_key'],'wx');
             $this->loginByOpenId($return['openid']);
             return $this->resultArray('success','登陆成功');
         } catch (ProcessException $e) {
@@ -60,6 +62,17 @@ class Wxapp extends Common
         if(!$user_info){
             Common::processException('not_bind');
         }
+        /** @var array $user_info */
+        if ($user_info["id"] != 1) {
+            // 查询node_id是否被禁用 如果被禁同样禁止登录
+            $node_info = (new Node)->where(["id" => $user_info["node_id"]])->find();
+            if (empty($node_info)) {
+                Common::processException("当前用户没有节点后台!!");
+            }
+            if ($node_info["status"] == "off") {
+                Common::processException("当前节点后台禁止登录!!");
+            }
+        }
         $user_info['type_name'] = 'node';
         $return_arr = [];
         /** @var array $user_info */
@@ -70,5 +83,43 @@ class Wxapp extends Common
         $return_arr['ip'] = 'wxapp';
         //设置session信息
         (new Login())->setLoginSession($return_arr);
+    }
+
+    /***
+     * @return array
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function bindopenid(){
+        $data = Request::instance()->post();
+        $rule = [
+            ["user_name", "require", "请填写用户名"],
+            ["password", "require", "请填写密码"],
+        ];
+        $validate = new Validate($rule);
+        try {
+            //验证字段
+            if (!$validate->check($data)) {
+                $error = $validate->getError();
+                /** @var string $error */
+                Common::processException($error);
+            }
+//            验证验证码
+//            if (!captcha_check($data["verify_code"])) {
+//                exception('验证码错误');
+//            };
+            //登录信息容器
+            $user_info = (new User())->checkUserLogin($data["user_name"], $data["password"]);
+            $user_info = (new User())->find($user_info['id']);
+            $user_info->openid = Session::get('openid','wx');
+            if(!$user_info->save()){
+                Common::processException('绑定失败，请重新尝试');
+            }
+            //如果存在
+            return $this->resultArray('success', '绑定成功');
+        } catch (ProcessException $exception) {
+            return $this->resultArray("failed", $exception->getMessage());
+        }
     }
 }
