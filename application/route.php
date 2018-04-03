68fdba65307d136e4465357fcf6150b234f15155
jingyang <linshuboy@qq.com>
2018-04-03 17:41:24 +0800

    wx

--- application/route.php
+++ application/route.php
@@ -367,6 +367,7 @@ Route::get('crontab', 'common/CrontabTask/index');
 
 Route::resource('wxarticle', 'common/wx/WxArticle');
 Route::resource('wxproduct', 'common/wx/WxProduct');
+Route::get('wximglistenname', 'common/wx/WxImgList');
 
 
 
