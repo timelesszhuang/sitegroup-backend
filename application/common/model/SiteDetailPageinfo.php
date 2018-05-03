7e16cbe4e48f27ac16417ba328053fb0604c10dc
yangjingzheng <1318911846@qq.com>
2018-05-03 15:04:47 +0800

    api

---
+++ application/common/model/SiteDetailPageinfo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+class SiteDetailPageinfo extends Model
+{
+    //
+    //只读字段
+    protected $readonly=["node_id"];
+    /**
+     * 获取所有数据
+     * @param $limit
+     * @param $rows
+     * @param $where
+     * @return array
+     * @author guozhen
+     */
+    //TODO oldfunction
+    public function getAll($limit, $rows, $where)
+    {
+        $count = $this->where($where)->count();
+        $data = $this->limit($limit, $rows)->where($where)->field('update_time',true)->order('id','desc')->select();
+        return [
+            "total" => $count,
+            "rows" => $data
+        ];
+
+    }
+}
