38689be9a70b914009f67ae4db81ea43d8288a5d
jingyang <linshuboy@qq.com>
2018-05-10 09:39:42 +0800

    将第二个数据库的配置独立出来,降低维护成本.

--- application/common/model/Hotnews.php
+++ application/common/model/Hotnews.php
@@ -10,28 +10,7 @@ class Hotnews extends Model
     // 设置当前模型对应的完整数据表名称
     protected $table = 'sc_hotnews';
 
-    protected $connection = [
-        // 数据库类型
-        'type' => 'mysql',
-        // 数据库连接DSN配置
-        'dsn' => '',
-        // 服务器地址
-        'hostname' => 'rdsfjnifbfjnifbo.mysql.rds.aliyuncs.com',
-//        // 数据库名
-        'database' => 'scrapy',
-        // 数据库用户名
-        'username' => 'scrapy',
-        // 数据库密码
-        'password' => '201671Zhuang',
-        // 数据库连接端口
-        'hostport' => '',
-        // 数据库连接参数
-        'params' => [],
-        // 数据库编码默认采用utf8
-        'charset' => 'utf8',
-        // 数据库表前缀
-        'prefix' => 'sc_',
-    ];
+    protected $connection = 'db2';
 
 
     /**
