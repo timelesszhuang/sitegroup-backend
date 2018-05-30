6b4ba044f27938085abda3d5a5b5a437cd7cba5b
yangjingzheng <1318911846@qq.com>
2018-05-30 10:37:40 +0800

    api

--- application/common/controller/Omapi.php
+++ application/common/controller/Omapi.php
@@ -19,48 +19,6 @@ class Omapi extends Common
     public function index()
     {
         $xmldata = file_get_contents('php://input');
-        //$xmldata = '544545';
-//
-//                $xmldata = <<<xmldata
-//<Cdr id="20920150929140022-0">
-//      <callid>49394</callid>
-//      <TimeStart>20150929135957</TimeStart>
-//      <Type>IN</Type>
-//      <Route>OP</Route>
-//      <CPN>13698612743</CPN>
-//      <CDPN>801</CDPN>
-//      <TimeEnd>20150929140022</TimeEnd>
-//      <Duration>4</Duration>
-//      <TrunkNumber>13698612743</TrunkNumber>
-//      <Recording>20150929/13698612743_316_20150929-140018_49394</Recording>
-//      </Cdr>
-//xmldata;
-
-//
-//$xmldata = <<<xmldata
-//<Cdr id="25920180530091121-0">
-//  <callid>61557</callid>
-//  <outer id="117" />
-//  <TimeStart>20180530091057</TimeStart>
-//  <Type>OU</Type>
-//  <Route>IP</Route>
-//  <CPN>801</CPN>
-//  <CDPN>013759405182</CDPN>
-//  <TimeEnd>20180530091121</TimeEnd>
-//  <Duration>527</Duration>
-//  <TrunkNumber>053188899463</TrunkNumber>
-//</Cdr>
-//xmldata;
-
-
-
-
-//        $xmldata = <<<xmldata
-//          <Event attribute="ANSWER">
-//          <ext id="334" />
-//          <visitor from="13698612743" />
-//          </Event>
-//xmldata;
         file_put_contents('11111.txt', $xmldata, FILE_APPEND);
         $this->analyse_data($xmldata);
     }
@@ -123,15 +81,11 @@ class Omapi extends Common
             }
             $data[$name]['val'] = empty($val) ? '' : $val;
         }
-        dump($data);die;
+        //dump($data);
         //获取   分机号码=>array(userid,flag)  跨分组调用模块
-        file_put_contents('test.log', "load", FILE_APPEND);
-        $extnum_nodeid_arr = Cache::remember('extnum_nodeid_arr',function(){
-            file_put_contents('test.log', "load_new", FILE_APPEND);
-            return  $this->get_extnum_nodeid_arr();
-        });
-        //dump($extnum_nodeid_arr);
-        file_put_contents('test.log', "load_over", FILE_APPEND);
+        //file_put_contents('test.log', "load", FILE_APPEND);
+        $extnum_nodeid_arr = $this->get_extnum_nodeid_arr();
+        //file_put_contents('test.log', "load_over", FILE_APPEND);
         //通话的唯一标识
         $cdr_data['callid'] = $data['callid']['val'];
         //$cdr_data['visitor'] = $data['visitor']['attr']['id'];
@@ -172,7 +126,7 @@ class Omapi extends Common
             $cdr_data['node_id'] = $extnum_nodeid_arr[$ext_num]['node_id'];
         } else {
             $cdr_data['node_id'] = 0;
-            exit();
+            //exit();
 //            file_put_contents('error.log', "{$flag} 地址，memcache 或者 数据库获取{$cdr_data['ext_num']}=>user_id  失败。\r\n", FILE_APPEND);
             //以后可以发送邮件报警  说明问题。
         }
@@ -188,7 +142,7 @@ class Omapi extends Common
             $cdr_data['rec_name'] = '';
         }
         $cdr_data['create_time'] = time();
-        dump($cdr_data);
+        //dump($cdr_data);
         return $cdr_data;
     }
 
