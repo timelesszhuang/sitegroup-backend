236f919f7f672dcde06d1c3d62fba0f72e258edd
yangjingzheng <1318911846@qq.com>
2018-05-31 09:43:05 +0800

    api

--- application/common/controller/VoiceCdr.php
+++ application/common/controller/VoiceCdr.php
@@ -34,7 +34,7 @@ class VoiceCdr extends Common
         foreach($data['rows'] as $key=>$datas){
             $data['rows'][$key]['timestart']=date('Y-m-d H:i:s',$datas['timestart']);
             $data['rows'][$key]['timeend']=date('Y-m-d H:i:s',$datas['timeend']);
-            $data['rows'][$key]['rec_name']=$datas['rec_name']?'http://voice.salesman.cc/lexiaoyi/'.$datas['rec_name'].'.mp3':'';
+            $data['rows'][$key]['rec_name']=$datas['rec_name']?'http://voice.salesman.cc/lexiaoyi/'.$datas['rec_name']:'';
         }
         return $this->resultArray('', '',$data);
     }
