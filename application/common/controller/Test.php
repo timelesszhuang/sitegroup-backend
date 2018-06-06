06ca834a8345a7d45db8752fdebd9f0030cd9f8e
yangjingzheng <1318911846@qq.com>
2018-06-06 10:20:47 +0800

    test

--- application/common/controller/Test.php
+++ application/common/controller/Test.php
@@ -13,17 +13,17 @@ class Test extends Controller
     public function __construct()
     {
         $token = $this->curl_getwx('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxba3dd8d2bdf50774&secret=80fe0e106a0d806b023970a08ac7b63c');
-        $this->token = json_decode($token)->access_token;
+        $this->token = json_decode($token);
     }
 
     public function test()
     {
 //        http://salesman.cc/index.php/Companywechat/Writedailyreport/index.html?flag=write
-        $redirect_url = urlencode('http://salesman.cc');
-        dump($redirect_url);
-        die;
+       // $redirect_url = urlencode('http://salesman.cc');
+        //dump($redirect_url);
+        //die;
 
-        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxba3dd8d2bdf50774&redirect_uri=http%3A%2F%2Fsalesman.cc&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
+        //$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxba3dd8d2bdf50774&redirect_uri=http%3A%2F%2Fsalesman.cc&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
         dump($this->token);
     }
 
