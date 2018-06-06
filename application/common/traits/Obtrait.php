635f40856bb7f906809402010cfa94fd88f07019
yangjingzheng <1318911846@qq.com>
2018-06-06 10:15:41 +0800

    test

--- application/common/traits/Obtrait.php
+++ application/common/traits/Obtrait.php
@@ -108,6 +108,26 @@ trait Obtrait
         return $data;
     }
 
+    public function curl_getwx($url)
+    {
+        //print_r($url);die;
+        //初始化
+        $curl = curl_init();
+        //设置抓取的url
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //设置头文件的信息作为数据流输出
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        //设置获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        //执行命令
+        $data = curl_exec($curl);
+        //关闭URL请求
+        curl_close($curl);
+        //显示获得的数据
+        return $data;
+    }
+
     /**
      * 图片文件转base64
      * @param $image_file
